77602ca5e20a24631e6d7b14dbbf6a6ef9a548a1
unibtc <unibtc@gmail.com>
2020-04-11 17:47:34 +0000

    : allow multiple dsn separate $sentry instance

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     SentrySpa
   ],
   sentry: {
-    dsn: 'https://fe8b7df6ea7042f69d7a97c66c2934f7@sentry.io/1429779',
+    dsn: ['https://fe8b7df6ea7042f69d7a97c66c2934f7@sentry.io/1429779', 'https://fe8b7df6ea7042f69d7a97c66c2934f7@sentry.io/1429779'],
     publishRelease: false,
     config: {}
   }
